5bf3f45b3b7534559f601e1077f5cf4f214ac4c3
JonusJackson <jonus.jackson1@gmail.com>
2026-05-19 15:50:59 -0700

    update files

--- rag_app.py
+++ rag_app.py
@@ -1,10 +1,15 @@
-print("STARTING FILE")
-
+from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 import sys
+from pathlib import Path
 
-load_dotenv()
+app = FastAPI()
+
+print("STARTING FILE")
+
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 api_key = os.getenv("GEMINI_API_KEY")
 
@@ -13,5 +18,3 @@ print("API KEY VALUE:", api_key)
 if not api_key:
     print("ERROR: GEMINI_API_KEY is missing.")
     sys.exit(1)
-
-print("SUCCESS")
